0c0bda255bb13cf36410ff2e87f8ef908824c1d6
matt <matt@zuso.ai>
2019-11-23 13:54:35 +0800

    調整 moveZeroes/answer.js

--- moveZeroes/answer.js
+++ moveZeroes/answer.js
@@ -3,19 +3,14 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function (nums) {
-  let length = nums.length;
-  let smallestNum;
-  let smallestNumIndex;
-  for (let i = 0; i < length; i++) {
-    const num = nums[i];
-    if(num == 0){
-      nums.shift();
-      nums.push(num);
+
+    let index = 0;
+    for (let times = 0; times < nums.length; times++) {
+        if(nums[index] === 0){
+            nums.splice(index, 1);
+            nums.push(0);
+        } else {
+            index++;
+        }
     }
-
-
-  }
 };
-
-
-console.log(moveZeroes([0, 1, 0, 3, 12]));
