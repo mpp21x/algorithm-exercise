b820c1e4f45fd0a3f75562e58f6a8f99090839e8
mpp21x <mpp21x@gmail.com>
2023-10-31 22:06:05 +0800

    refactor 3-longest-substring-without-repeating

--- leetcode/String/3-longest-substring-without-repeating-characters/ts/longest-substring-without-repeating-characters.ts
+++ leetcode/String/3-longest-substring-without-repeating-characters/ts/longest-substring-without-repeating-characters.ts
@@ -2,13 +2,11 @@ export function lengthOfLongestSubstring(s: string): number {
 	let currentStr = '';
 	let maxStr = '';
 	for (let i = 0; i < s.length; i++) {
-		const char = s[i];
-		const index = currentStr.indexOf(char);
-		if(index !== -1){
-			currentStr = currentStr.slice(index + 1);
+		if(currentStr.indexOf(s[i]) !== -1){
+			currentStr = currentStr.slice(currentStr.indexOf(s[i]) + 1);
 		}
 
-		currentStr += char;
+		currentStr += s[i];
 		if(maxStr.length < currentStr.length){
 			maxStr = currentStr;
 		}
@@ -16,13 +14,4 @@ export function lengthOfLongestSubstring(s: string): number {
 
 	return maxStr.length;
 }
-
-/*
-檢討
-* 要的不是第一個長度，是每個字元都是為起點的長度
-* 當你看不懂別人的答案時，就有徵兆自己不懂了
-* 切得不夠細
- */
-
-
 
